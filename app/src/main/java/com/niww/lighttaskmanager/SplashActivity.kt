f3690529761f54610ddaeb218411cb6659caeb8f
Sosorevgm <sosorevgm@gmail.com>
2020-07-30 14:19:41 +0300

    Add SplashActivity and drawable file for SplashActivity theme. Add AuthorizationActivity with the possibility of authorization. Change manifest - SplashActivity is launcher activity now.

---
+++ app/src/main/java/com/niww/lighttaskmanager/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.niww.lighttaskmanager
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        checkAuth()
+    }
+
+    private fun checkAuth() {
+        if (auth.currentUser == null) {
+            val authorizationIntent = Intent(this, AuthorizationActivity::class.java)
+            startActivity(authorizationIntent)
+            finish()
+        } else {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }
+    }
+}
